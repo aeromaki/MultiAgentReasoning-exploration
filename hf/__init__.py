8c0d500c15030f98da1995f65a87ea68207ac0c2
-3σ <yurippe@m3sigma.net>
2024-07-18 04:24:26 +0900

    -

--- hf/__init__.py
+++ hf/__init__.py
@@ -9,8 +9,13 @@ def create_hf_chain(model_name: str, max_new_tokens: int = 512) -> HuggingFacePi
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=CACHE_DIR)
     model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=CACHE_DIR, device_map="auto")
 
-    model.generation_config.pad_token_id = tokenizer.pad_token_id
-
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=max_new_tokens, device_map="auto")
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=max_new_tokens,
+        device_map="auto",
+        pad_token_id=tokenizer.eos_token_id
+    )
 
     return HuggingFacePipeline(pipeline=pipe)
